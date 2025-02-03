54e0a1cfc53640e9411aa0b219d1bcc23b6dadd1
Muhammad Subhan Nadeem <sn9273671@gmail.com>
2025-02-03 02:05:01 +0000

    More Updates

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,11 @@
 import { createRoot } from "react-dom/client";
 import Main_router from "./main_router/main_router";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import {ReactQueryDevtools} from "@tanstack/react-query-devtools";
 let queryClient = new QueryClient();
 createRoot(document.getElementById("main")).render(
   <QueryClientProvider client={queryClient}>
+    <ReactQueryDevtools initialIsOpen={false}/>
     <Main_router />
   </QueryClientProvider>
 );
