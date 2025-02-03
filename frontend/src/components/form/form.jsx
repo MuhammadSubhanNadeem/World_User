1e9322113f365ecc5d7428d8fb7b852773605949
Muhammad Subhan Nadeem <sn9273671@gmail.com>
2025-02-03 07:02:54 +0000

    More Updates

--- frontend/src/components/form/form.jsx
+++ frontend/src/components/form/form.jsx
@@ -1,23 +1,47 @@
 import { motion } from "motion/react";
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 
 function Form() {
   const inputName = useRef(null);
   const inputEmail = useRef(null);
+  const userImage = useRef(null);
+  const [userImgFile, setUserImgFile] = useState({
+    img_set: false,
+    img_name: "",
+  });
   function submitData(defs) {
     defs.preventDefault();
-    let data = {
-      name: inputName.current.value,
-      email: inputEmail.current.value,
-    };
-    fetch("http://localhost:3000/", {
+    let formData = new FormData();
+    let userImageFile = userImage.current.files[0];
+    formData.append("name", inputName.current.value);
+    formData.append("email", inputEmail.current.value);
+    formData.append("userImg", userImageFile);
+    fetch(`${import.meta.env.VITE_BACKEND_URL}/`, {
       method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(data),
+      body: formData,
     })
       .then((raw_res) => raw_res.json())
       .then((res) => {
         console.log(res);
+        if (res.cause === "Duplication Err!") {
+          inputName.current.value = "";
+          inputEmail.current.value = "";
+          userImage.current.value = "";
+          inputName.current.style.border = "1px solid red";
+          inputEmail.current.style.border = "1px solid red";
+          userImage.current.style.border = "1px solid red";
+          setTimeout(() => {
+            inputName.current.style.border = "1px solid white";
+            inputEmail.current.style.border = "1px solid white";
+            userImage.current.style.border = "1px solid white";
+          }, 1500);
+          window.alert("❌ You Are Already ON Fire! Get Out On All User Data 😄!");
+        } else if (res.img_upload && res.write){
+          inputName.current.value = "";
+          inputEmail.current.value = "";
+          userImage.current.value = "";
+          window.alert("✅ You Are Already ON Fire! Get Out On All User Data!");
+        }
       });
   }
   let data = {
@@ -27,12 +51,13 @@ function Form() {
   return (
     <div className="w-full h-full flex items-center justify-center">
       <motion.form
-        action="http://localhost:3000/"
+        action={`${import.meta.env.VITE_BACKEND_URL}/`}
         onSubmit={(defs) => submitData(defs)}
         method="post"
         exit={{ y: -100 }}
+        encType="multipart/form-data"
         transition={{ duration: 0.5 }}
-        className="w-max h-max border flex flex-col gap-[50px] bg-[#15d47b] p-[50px] text-white rounded-2xl shadow-[5px_10px_25px] shadow-[#d2c1ff]"
+        className="w-max h-max border flex flex-col bg-[#15d47b] p-[50px] text-white rounded-2xl shadow-[5px_10px_25px] shadow-[#d2c1ff]"
       >
         <motion.input
           type="text"
@@ -41,8 +66,9 @@ function Form() {
           initial="initial"
           animate="animation"
           name="name"
-          className="border rounded-[5px] w-[250px] pl-[15px] pt-1 pb-1 outline-none text-white"
+          className="border mb-6 rounded-[5px] w-[250px] pl-[15px] pt-1 pb-1 outline-none text-white"
           placeholder="Enter Your Name"
+          required
         />
         <motion.input
           type="email"
@@ -51,12 +77,42 @@ function Form() {
           initial="initial"
           animate="animation"
           variants={data}
-          className="border rounded-[5px] w-[250px] pl-[15px] pt-1 pb-1 outline-none"
+          className="border mb-6 rounded-[5px] w-[250px] pl-[15px] pt-1 pb-1 outline-none"
           placeholder="Enter Your Name"
+          required
         />
+        <input
+          type="file"
+          id="user_img"
+          ref={userImage}
+          onChange={(defs) => {
+            console.log(defs.target.files[0]);
+            setUserImgFile((prev) => {
+              return {
+                ...prev,
+                img_name: defs.target.files[0].name,
+                img_set: true,
+              };
+            });
+          }}
+          className="w-[250px] overflow-hidden hidden"
+          required
+          accept=".jpg,.png,.gif,.webp,.jpeg"
+        />
+        <label
+          htmlFor="user_img"
+          className="w-[50%] h-[45px] mb-1 text-center transition-all duration-150 ease-in content-center cursor-pointer hover:bg-[rgba(255,255,255,0.1)] border-[white] border-[1px] rounded-[5px]"
+        >
+          Upload Image
+        </label>
+        {userImgFile.img_set ? (
+          <div className="w-[180px] mb-1 h-fit text-start overflow-ellipsis overflow-hidden text-nowrap text-[13px]">
+            {userImgFile.img_name}
+          </div>
+        ) : null}
         <button
           type="submit"
-          className="border rounded-[5px] w-[250px] py-1 transition-all active:scale-[0.95] duration-75 ease-initial outline-none cursor-pointer bg-cyan-700 hover:bg-cyan-900 text-white font-extrabold"
+          className="border rounded-[5px] mt-12 w-[250px] py-1 transition-all active:scale-[0.95] duration-75 ease-initial outline-none cursor-pointer bg-cyan-700 hover:bg-cyan-900 text-white font-extrabold"
         >
           Submit
         </button>
--- frontend/src/components/userData/userData.jsx
+++ frontend/src/components/userData/userData.jsx
@@ -20,26 +20,55 @@ async function fetchUsersData() {
 }
 function UserData() {
   let { data, isLoading, isError, error } = useQuery({
-    queryKey: ['userData'],
+    queryKey: ["userData"],
     queryFn: fetchUsersData,
   });
   return (
     <>
-      <div className="w-full bg-cyan-600 h-[80%] flex items-center justify-center gap-5 p-[75px] overflow-y-scroll overflow-x-hidden flex-wrap">
-        {(!isLoading) ? data.map((eachData, index) => {
-          return (
-            <div
-              key={index}
-              className="border-[1px] border-[rgba(255,255,255,0.15)] border-solid p-5 bg-cyan-600 flex flex-col gap-3 rounded-2xl shadow-[0px_0px_15px] shadow-[#0000002d]"
-            >
-              <span className="text-white text-[18px]">{eachData.name}</span>
-              <hr className="w-full border-none bg-[rgba(255,255,255,0.25)] h-[0.5px]" />
-              <span className="text-white text-[18px]">{eachData.email}</span>
-            </div>
-          );
-        }) : (isError) ? <h1 className="text-white font-bold text-[32px]">Something Goes Wrong!!</h1>: <Loading />}
+      <div className="w-full bg-cyan-600 h-[80%] flex items-center justify-center gap-5 p-[15px] overflow-y-scroll overflow-x-hidden flex-wrap">
+        {!isLoading ? (
+          data["users_data"].map((eachUser, userIndex) => {
+            return eachUser.name && eachUser.email ? (
+              <div
+                key={userIndex}
+                className="border-[1px] border-[rgba(255,255,255,0.15)] w-[320px] border-solid p-5 overflow-hidden bg-cyan-600 flex flex-col gap-3 rounded-2xl shadow-[0px_0px_15px] shadow-[#0000002d]"
+              >
+                {data["img_parsed_data"].map((each_user_img, userImgIndex) => {
+                  if (each_user_img.user_email === eachUser.email) {
+                    return (
+                      <div
+                        className="w-[70px] h-[70px] p-[2px] border-[2px] border-[white] overflow-hidden rounded-[50%] relative flex items-center justify-start mb-2"
+                        key={userImgIndex}
+                      >
+                        <img
+                          src={each_user_img.user_cloud_img}
+                          className="w-full h-full rounded-[50%] object-cover"
+                          alt="Your Damn Face"
+                        />
+                      </div>
+                    );
+                  }
+                })}
+                <span className="text-white text-[18px] overflow-ellipsis text-nowrap">{eachUser.name}</span>
+                <hr className="w-full border-none bg-[rgba(255,255,255,0.25)] h-[0.5px]" />
+                <span className="text-white text-[18px]">{eachUser.email}</span>
+              </div>
+            ) : null;
+          })
+        ) : isError ? (
+          <h1 className="text-white font-bold text-[32px]">
+            Something Goes Wrong!!
+          </h1>
+        ) : (
+          <Loading />
+        )}
       </div>
-      <a href="https://lordicon.com/" className="absolute bottom-4 right-12 text-[rgba(255,255,255,0.6)] hover:underline">Animated icons by Lordicon.com</a>
+      <a
+        href="https://lordicon.com/"
+        className="absolute bottom-4 right-12 text-[rgba(255,255,255,0.6)] hover:underline"
+      >
+        Animated icons by Lordicon.com
+      </a>
     </>
   );
 }
